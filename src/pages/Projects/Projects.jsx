9029fd22cf55986c63a347af38740728cf93a2ce
nathalie-jane <nathalie.zheng@gmail.com>
2026-06-01 20:11:50 +0200

    feat(projects): add project card toggle
    - Add state for showing and hiding project cards
    - Add toggle button for project cards

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -5,20 +5,33 @@
     and displays Project cards
 ====================================== */
 
+import { useState } from "react";
+
 import "./Projects.css";
 import ProjectCard from "../../components/ProjectCard/ProjectCard";
 import projects from "../../data/projects";
 
 function Projects() {
+	const [showProjects, setShowProjects] = useState(false);
+
+	const toggleProjects = () => {
+		setShowProjects(function (isVisible) {
+			return !isVisible;
+		});
+	};
+
 	return (
 		<section className="projects">
 			<div className="projects__inner">
 				<div className="projects__heading">
 					<h1 className="projects__title">Projects</h1>
 					<p className="projects__intro">A collection of some of my recent work</p>
+					<button className="projects__button" type="button" onClick={toggleProjects}>
+						{showProjects ? "Hide Projects" : "Show Projects"}
+					</button>
 				</div>
 
-				<div className="projects__collection">
+				<div className={showProjects ? "projects__collection projects__collection--visible" : "projects__collection"}>
 					{projects.map(function (project) {
 						return (
 							<ProjectCard
